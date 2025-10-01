c1b433b2927b00e7c711963a01d5967ce0863560
Gabriel Marvullo <gabrieelrossini23@hotmail.com>
2025-10-01 19:09:05 -0300

    Concluindo o texto-base, opção de fazer otimizações no código para aprendizado e treino

--- src/semana1/Account2Test.java
+++ src/semana1/Account2Test.java
@@ -1,5 +1,40 @@
 package src.semana1;
 
-public class Account2Test {
-    
+// Exemplo 5 do texto-base: Account2Test.java
+// Entrada e saída de números de ponto flutuante com objeto Account
+
+import java.util.Scanner;
+
+public class Account2Test 
+{
+    public static void main(String[] args)
+    {
+        Account2 account1 = new Account2("Jane green", 50.00);
+        Account2 account2 = new Account2("John Blue", -7.53);
+
+        // exibe saldo inicial de cada objeto
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+
+        // cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter deposit amount for account1: "); // prompt
+        double depositAmount = input.nextDouble(); // obtém a entrada do usuário
+        System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount); // adiciona o saldo de account1
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: "); // prompt
+        depositAmount = input.nextDouble(); // obtém a entrada do usuário
+        System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+        account2.deposit(depositAmount); // adiciona o saldo de account1
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+    }
 }
