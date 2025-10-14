90325c9930374eb9f62c7e9abb7620af3e6de1a1
Gabriel Marvullo <gabrieelrossini23@hotmail.com>
2025-10-14 19:41:21 -0300

    criação da classe Caneta e início da CanetaTeste (herança)

---
+++ src/semana3/Caneta.java
@@ -0,0 +1,51 @@
+package src.semana3;
+
+// Exercício de atividade complementar: Caneta.java
+// Exercitando conceito de herança e polimorfismo em POO
+
+public class Caneta {
+
+    // Atributos
+
+    private String marca;
+    private String cor;
+    private double tamanho;
+    
+    // Construtor
+
+    public Caneta(String marca, String cor, double tamanho) {
+        this.marca = marca;
+        this.cor = cor;
+        this.tamanho = tamanho;
+    }
+
+    // Métodos get e set
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String toString() {
+        return String.format ("A caneta é da marca %s, da cor %s e tem %.2f de tamanho", marca, cor, tamanho);
+    }
+}
---
+++ src/semana3/CanetaTeste.java
@@ -0,0 +1,8 @@
+package src.semana3;
+
+public class CanetaTeste extends Caneta {
+    public CanetaTeste(String marca, String cor, double tamanho){
+        super(marca, cor, tamanho);
+    }
+    
+}
