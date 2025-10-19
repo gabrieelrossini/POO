e4f624a6901509cecfb6fc33efb05589e63ecde6
marvullo <gabrieelrossini23@hotmail.com>
2025-10-18 21:28:45 -0300

    testando pull no fedora

--- src/semana1/Dialog1.java
+++ src/semana1/Dialog1.java
@@ -5,6 +5,6 @@ import javax.swing.JOptionPane;
 public class Dialog1 {
     public static void main(String[] args) {
         // exibe um diálogo com uma mensagem
-        JOptionPane.showMessageDialog(null, "Welcome to Java");
+        JOptionPane.showMessageDialog(null, "Testando Pull no Fedora");
     }
 }
